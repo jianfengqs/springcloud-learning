67b6c299988410b283cd56cb2c87b349e1829fef
zhaijianfeng <jianfengqs@163.com>
2022-11-02 10:46:58 +0800

    fix:调整redis

---
+++ test-model/service/src/main/java/com/qs/service/TestService.java
@@ -0,0 +1,10 @@
+package com.qs.service;
+
+/**
+ * 测试服务
+ *
+ * @author zhaijianfeng
+ * @date 2022/08/16
+ */
+public interface TestService {
+}
---
+++ test-model/service/src/main/java/com/qs/service/impl/TestServiceImpl.java
@@ -0,0 +1,12 @@
+package com.qs.service.impl;
+
+import com.qs.service.TestService;
+
+/**
+ * 测试服务impl
+ *
+ * @author zhaijianfeng
+ * @date 2022/08/16
+ */
+public class TestServiceImpl implements TestService {
+}
