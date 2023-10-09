d849493e7aef2c974efb853e6d597cc60f915ae8
zhaijianfeng <jianfengqs@163.com>
2023-10-09 14:34:09 +0800

    feat：新增clean-boot项目

---
+++ clean-module/service-module/src/main/java/cleanboot/CleanBootApplication.java
@@ -0,0 +1,18 @@
+package cleanboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用程序
+ *
+ * @author feishi
+ * @date 2021/10/28
+ */
+@SpringBootApplication
+public class CleanBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanBootApplication.class, args);
+    }
+}
